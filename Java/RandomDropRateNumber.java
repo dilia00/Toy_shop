38600e4bd331d29ebdd9efe1a7b39e74f5051a6e
dilia00 <dina_xenia@mail.ru>
2023-04-12 08:09:02 +0300

    выполненое задание 2

---
+++ Java/RandomDropRateNumber.java
@@ -0,0 +1,10 @@
+package Java;
+
+import java.util.Random;
+
+public class RandomDropRateNumber {
+    public static int rnd() {
+        Random random = new Random();
+        return random.nextInt(100);
+    }
+}
